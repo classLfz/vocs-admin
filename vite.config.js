a3e164c48b2ce8526556526dc9cb726e75292fe5
classlfz <classlfz@qq.com>
2021-04-26 22:47:51 +0800

    :bug: fix vite base

--- vite.config.js
+++ vite.config.js
@@ -3,22 +3,23 @@ import vue from '@vitejs/plugin-vue'
 import styleImport from 'vite-plugin-style-import'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    styleImport({
-      libs: [
-        {
-          libraryName: 'element-plus',
-          esModule: true,
-          ensureStyleFile: true,
-          resolveStyle: (name) => {
-            return `element-plus/lib/theme-chalk/${name}.css`;
-          },
-          resolveComponent: (name) => {
-            return `element-plus/lib/${name}`;
-          },
-        }
-      ]
-    })
-  ]
+	base: '/vocs-admin/',
+	plugins: [
+		vue(),
+		styleImport({
+			libs: [
+				{
+					libraryName: 'element-plus',
+					esModule: true,
+					ensureStyleFile: true,
+					resolveStyle: (name) => {
+						return `element-plus/lib/theme-chalk/${name}.css`;
+					},
+					resolveComponent: (name) => {
+						return `element-plus/lib/${name}`;
+					},
+				}
+			]
+		})
+	]
 })
